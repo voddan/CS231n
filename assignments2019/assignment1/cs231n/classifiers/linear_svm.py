12ede7227882ab9550487f63f77b51b563f16adc
voddan <dgvodopyan@gmail.com>
2019-08-15 23:43:42 +0300

    Assignment1, Q2: SVM, vectorised gradient - WIP!

--- assignments2019/assignment1/cs231n/classifiers/linear_svm.py
+++ assignments2019/assignment1/cs231n/classifiers/linear_svm.py
@@ -119,8 +119,37 @@ def svm_loss_vectorized(W, X, y, reg):
     # loss.                                                                     #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    num_dim = X.shape[1]
 
-    pass
+    scores = X.dot(W)  # (N, C)
+    correct_scores = scores[range(num_train), y]
+    margin = (scores.T - correct_scores + 1).T  # (N, C)
+    assert margin.shape == (num_train, num_classes)
+
+    # dScores = X.T.dot(margin > 0)
+    # dScores = X  #(N, D)
+    # dCorrect_scores = dScores[range(num_train), y]
+    #
+    # dMargin = (dScores.T - dCorrect_scores).T
+    #
+    # dMarginClipped = dMargin * (margin > 0) #todo
+    # dW += np.sum(dMarginClipped)
+
+
+
+
+    dScore = X.T.dot(margin > 0)
+    dW += dScore
+
+    # sum = X.T.dot(np.sum(margin > 0, axis=1).reshape(num_train, 1))
+
+    for i in range(num_train): #N
+        sum = X.T * np.sum(margin > 0, axis=1)[i]
+        assert sum.shape == (num_dim, num_train), sum.shape
+        dW[:, y[i]] -= sum[:, i]
+
+    dW /= num_train
+    dW += reg * 2 * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
